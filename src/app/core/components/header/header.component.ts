8edd58c1c7607a73f0cf085cf0e55710d27f9aec
Michael Rayson <mrayson5129@gmail.com>
2022-03-16 10:41:05 -0400

    added navigation buttons to header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavButton } from '../../models/nav-button';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  navButtons: NavButton[] = [];
+  isMobile: boolean = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    // Create navButtons
+    this.navButtons.push({
+      text: 'List a ride',
+      icon: 'edit',
+      route: 'rides/list'
+    });
+    this.navButtons.push({
+      text: 'Find a ride',
+      icon: 'search',
+      route: 'rides/find'
+    });
+    this.navButtons.push({
+      text: 'Account',
+      icon: 'account_circle',
+      route: 'account/settings'
+    });
+    // check window size
+    if (window.innerWidth < 768) this.isMobile = true;
+  }
+
+  onResize(event: any): void {
+    this.isMobile = this.isSmallScreen();
+  }
+
+  isSmallScreen(): boolean {
+    if (window.innerWidth < 768) return true;
+    return false;
+  }
+
+  route(route: string): void {
+    this.router.navigateByUrl(route);
   }
 
 }
---
+++ src/app/core/models/nav-button.ts
@@ -0,0 +1,5 @@
+export interface NavButton {
+    text: string,
+    icon?: string,
+    route: string,
+}
